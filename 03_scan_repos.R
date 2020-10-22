b55c0136713eb8c9628432ed19687b9057f53f00
Greg Sutcliffe <github@emeraldreverie.org>
2020-10-22 10:44:28 +0100

    Don't re-parse the projects list from 01, use a regex for word detection

--- 03_scan_repos.R
+++ 03_scan_repos.R
@@ -4,38 +4,21 @@ library(pins)
 library(tidyverse)
 library(here)
 library(furrr)
-plan(multiprocess, workers=2)
 
 setwd(here())
 
 pins::board_register_local(name = 'conscious_lang', cache = '/tmp')
 
-projects <- pin_get('cl_projects', board = 'conscious_lang') %>%
-  # temporary, github only, other styles to come
-  filter(str_detect(repo,'github.com')) %>%
-  # split up the path so we can use it for things
-  mutate(url   = repo,
-         path  = str_split(url,'/'),
-         parts = map_int(path, ~{ .x %>%
-                                    unlist() %>%
-                                    length() })
-  ) %>%
-  filter(parts == 5) %>% # habndle orgs (4 parts) later
-  mutate(org  = map_chr(path, ~{ unlist(.x) %>%
-                                   tail(2) %>%
-                                   head(1) }),
-         repo = map_chr(path, ~{ unlist(.x) %>%
-                                   tail(1) })
-  ) %>%
+repos <- pin_get('cl_results', board = 'conscious_lang') %>%
   select(url, org, repo)
 
-count_words <- function(org, repo, word) {
+count_words <- function(org, repo, regx) {
   # Search path for this repo
-  path = here('clones',org,repo)
+  path = here('clones', org, repo)
 
   # This is very ugly, but ag returns exit 1 on match-not-found
   suppressWarnings(
-    system2('ag',c('-c', word, path), stdout = TRUE, stderr = FALSE)
+    system2('ag',c('-c', regx, path), stdout = TRUE, stderr = FALSE)
   ) -> res
 
   # Ag2 vs Ag1
@@ -53,13 +36,14 @@ count_words <- function(org, repo, word) {
 }
 
 # Count words in clone
-projects %>%
-  mutate(blacklist = future_map2_int(org, repo, count_words, 'blacklist', .progress = TRUE),
-         whitelist = future_map2_int(org, repo, count_words, 'whitelist', .progress = TRUE),
-         master    = future_map2_int(org, repo, count_words, 'master',    .progress = TRUE),
-         slave     = future_map2_int(org, repo, count_words, 'slave',     .progress = TRUE)
-  ) -> projects
+plan(multiprocess, workers=2)
+repos %>%
+  mutate(blacklist = future_map2_int(org, repo, count_words, 'black[-_]?list', .progress = TRUE),
+         whitelist = future_map2_int(org, repo, count_words, 'white[-_]?list', .progress = TRUE),
+         master    = future_map2_int(org, repo, count_words, 'master',         .progress = TRUE),
+         slave     = future_map2_int(org, repo, count_words, 'slave',          .progress = TRUE)
+  ) -> repos
 
-projects <- projects %>% filter(blacklist + whitelist + master + slave > 0)
-
-pin(projects,name='cl_results', board = 'conscious_lang')
+repos %>%
+  filter(blacklist + whitelist + master + slave > 0) %>%
+  pin(name='cl_results', board = 'conscious_lang')
